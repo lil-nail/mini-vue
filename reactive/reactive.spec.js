64700dda23da742d39986b69562d498a42e6c5f7
panther <22311632+lil-nail@users.noreply.github.com>
2023-07-06 13:45:59 +0000

    🧪 test(reactive): add reactive & effect version1 & version2 spec

--- reactive/reactive.spec.js
+++ reactive/reactive.spec.js
@@ -1,8 +1,11 @@
 import { expect, test, describe } from 'vitest'
-import { reactive, effect } from './reactive'
+import { useReactiveV1, useReactiveV2 } from './reactive'
 
 describe('reactive', () => {
-  test('effect demo', () =>　{
+  test('effect simple demo', () =>　{
+
+    const { reactive, effect } = useReactiveV1();
+
     const person = reactive({
       age: 13,
       sex: 'man',
@@ -10,10 +13,83 @@ describe('reactive', () => {
     })
 
     let resultAge = null
-    effect(() => { resultAge = person.age })
+    effect(() => {
+      console.info('effect')
+      resultAge = person.age
+    })
     
     expect(resultAge).toEqual(13)
     person.age = 20
     expect(resultAge).toEqual(20)
+  })
+
+  test('effect version 1.0 demo', () =>　{
+
+    const { reactive, effect } = useReactiveV1();
+
+    const person = reactive({
+      age: 13,
+      sex: 'man',
+      isStudent: true,
+    })
+
+    let resultAge = null
+    effect(() => {
+      console.info('v1 - effect1')
+      resultAge = person.age
+    })
+    
+    const person2 = reactive({
+      age: 11,
+      sex: 'woman',
+      isStudent: true,
+    })
+
+    let resultAge2 = null
+    effect(() => {
+      console.info('v1 - effect2')
+      resultAge2 = person2.age
+    })
+    
+    expect(resultAge).toEqual(13)
+    expect(resultAge2).toEqual(11)
+    person.age = 20
+    expect(resultAge).toEqual(20)
+    expect(resultAge2).toEqual(11)
+  })
+
+  test('effect version 2.0 demo', () =>　{
+
+    const { reactive, effect } = useReactiveV2();
+
+    const person = reactive({
+      age: 13,
+      sex: 'man',
+      isStudent: true,
+    })
+
+    let resultAge = null
+    effect(() => {
+      console.info('version2 - effect1')
+      resultAge = person.age
+    })
+    
+    const person2 = reactive({
+      age: 11,
+      sex: 'woman',
+      isStudent: true,
+    })
+
+    let resultAge2 = null
+    effect(() => {
+      console.info('version2 - effect2')
+      resultAge2 = person2.age
+    })
+    
+    expect(resultAge).toEqual(13)
+    expect(resultAge2).toEqual(11)
+    person.age = 20
+    expect(resultAge).toEqual(20)
+    expect(resultAge2).toEqual(11)
   })
 })
