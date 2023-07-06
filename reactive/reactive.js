045e1d19bd90229bd106102acb5df89680d6938d
panther <22311632+lil-nail@users.noreply.github.com>
2023-07-06 13:45:22 +0000

    ✨ feat(reactive): complete version1.0 & 2.0

--- reactive/reactive.js
+++ reactive/reactive.js
@@ -1,26 +1,92 @@
-export const effects = [];
+// version 1.0
+export const useReactiveV1 = () => {
 
-export const effect = (fn) => {
-  effects.push(fn)
-  fn()
+  const effects = [];
+
+  const effect = (fn) => {
+    effects.push(fn)
+    fn()
+  }
+
+  const reactive = (object) => {
+    let reactiveObject = new Proxy(object, {
+      get (target, property, receiver) {
+        console.info('version1 - get value:', target[property])
+        return target[property]
+      },
+      set (target, property, value, receiver) {
+        console.info('version1 - set value:', target, property, value)
+        target[property] = value;
+        // traverser effects & execute function
+        for (const effectFn of effects) {
+          if (effectFn) effectFn()
+        }
+        return target
+      }
+    })
+
+    return reactiveObject
+  }
+
+  return {
+    effects,
+    effect,
+    reactive,
+  }
 }
 
-export const reactive = (object) => {
-  let reactiveObject = new Proxy(object, {
-    get (target, property, receiver) {
-      console.info('get value:', target[property])
-      return target[property]
-    },
-    set (target, property, value, receiver) {
-      console.info('set value:', target, property, value)
-      target[property] = value;
-      // traverser effects & execute function
-      for (const effectFn of effects) {
-        if (effectFn) effectFn()
-      }
-      return target
-    }
-  })
+// first. every set will execute all effect, but I wish it just execute the same dependence object and property
+// second. but if 100 object & 100 effect, the effect will execute 100 x 100 = 10000 times
+// third. every effect will execute 
 
-  return reactiveObject
+// version 2.0
+
+export const useReactiveV2 = () => {
+  
+  const effects = new Map();
+
+  let currentEffect = null;
+
+  const effect = (fn) => {
+    if (effects.has(fn)) effects.set(fn)
+    currentEffect = fn;
+    fn();
+    currentEffect = null;
+  }
+
+  const reactive = (object) => {
+    let observed = new Proxy(object, {
+      get (target, property, receiver) {
+        console.info('version2 - get value:', target[property])
+        if (currentEffect) {
+          if (!effects.has(target))
+            effects.set(target, new Map);
+          if (!effects.get(target).has(property))
+            effects.get(target).set(property, new Array)
+          effects.get(target).get(property).push(currentEffect)
+        }
+        return target[property]
+      },
+      set (target, property, value, receiver) {
+        console.info('version2 - set value:', target, property, value)
+        target[property] = value;
+        // traverser effects & execute function
+        if (effects.has(target) && effects.get(target).has(property)) {
+          for (let effect of effects.get(target).get(property)) {
+            effect()
+          }
+        }
+        return value;
+      }
+    })
+
+    return observed
+  }
+
+  return {
+    effects,
+    currentEffect,
+    effect,
+    reactive,
+  }
 }
